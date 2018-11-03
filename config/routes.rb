910d0a65455e462de5bc38277c7b8e4bc404411a
RezaZR <frans.van3@gmail.com>
2018-11-03 12:06:57 +0700

    added administrate for admin panel

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :buyer_goods
+      resources :goods
+      resources :stocks
+      resources :users
+
+      root to: "buyer_goods#index"
+    end
   # root :to => "application#index"
   
   # get "sejarah" => "histories#index"
